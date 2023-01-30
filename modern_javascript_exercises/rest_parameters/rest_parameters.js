419e71d68965d3d71ce986c330b30aa911d01fec
Matthew Hanson <dingbat91@gmail.com>
2023-01-30 12:01:24 +0000

    Exercises Completed

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,10 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(...nums) {
+	// Add a loop here
+	return nums.reduce((total, num) => {
+		return total + num;
+	});
 }
 
 add(1, 2, 3, 4, 5);
-
-module.exports = add;
